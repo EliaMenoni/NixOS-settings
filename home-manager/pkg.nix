7e63e5f832f55228ac11cc1cf2d20d6813df8507
EliaMenoni <eliamenoni@emenoni.eu>
2024-07-14 19:33:28 +0200

    Installazione VIM

--- home-manager/pkg.nix
+++ home-manager/pkg.nix
@@ -7,11 +7,11 @@
     ./app_conf/git.nix
     ./app_conf/zsh.nix
     ./app_conf/firefox.nix
+    ./app_conf/nixvim.nix
   ];
 
   home.packages = with pkgs; [
     typora
-
     # FONTS
     font-awesome
     fira-code
